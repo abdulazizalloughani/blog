0943ef3188a7b7dbd1feec150ff6c4277769bafb
abdulazizalloughani <abdulaziz.alloughani@cba.ku.edu.kw>
2021-12-09 11:02:20 +0300

    aa

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import post 
+
+class postadmin(admin.ModelAdmin):
+    list_display = ('title', 'states' , 'created_at')
+    list_filter = ('states', 'created_at')
+    search_fields = ['title', 'body',]
+admin.site.register(post , postadmin)
--- blogproject/blog/models.py
+++ blogproject/blog/models.py
@@ -2,9 +2,16 @@ from django.db import models
 
 # Create your models here.
 class post(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    STATES = (
+        (0 ,"Unpublished" ),
+        (1 ,"published" ),
+    )
+    title = models.CharField(max_length=100 , unique=True )
+    slug = models.SlugField(max_length=100 , unique=True)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    states = models.IntegerField()
+    states = models.IntegerField(choices= STATES, default= 0)
+
+    def __str__(self):
+        return f"{self.title} : created at {self.created_at} "
